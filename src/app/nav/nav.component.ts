1505e7abcc516c16139d87efd9098f3f5f2863bd
Antonio <antonio75@gmail.com>
2018-02-01 21:40:03 +0100

    Added login and register

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/Auth.service';
+import { error } from 'util';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(data => {
+      console.log('logged in succesfully');
+    }, error => {
+      console.log('fail login');
+    });
+  }
+
+  logout() {
+    this.authService.userToken = null;
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
+  logedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+}
